f9aa6aecac390d4431626731f59e7bbe2a6aa7f3
paulogdm <paulogdemitri@gmail.com>
2018-05-26 18:13:16 -0300

    Fixes and file remove
    * Fix on c.png
    * package-lock.json removed
    * texts.js added

---
+++ texts.js
@@ -0,0 +1,253 @@
+const locale = {
+  en: {
+    flag: '/assets/USA.png',
+    sections: {
+      personal: {
+        title: 'Personal',
+        subtitle: 'A few things about me.',
+        pcmasterrace: `I've always had a PC at home. <br>
+        I assembled my very own first PC in 2010. <br>
+        PC Master Race FTW. <br>
+        <a target="_blank" href="https://www.reddit.com/r/pcmasterrace/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        crypto: `I started with Litecoin and Bitcoin in 2012.
+        I only went to study the technology after 2015. <br>
+        Ethereum is awesome! <br>
+        If you say to me: "I don't like Bitcoin, but blockchain is very interesting." I will die discreetly inside.<br>
+        <a target="_blank" href="https://www.reddit.com/r/Bitcoin/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        overwatch: `I love to play Overwatch! <br>
+        My "Career High" is 4075. <br>
+        I consider myself a flex player, but I play better as a tank or support. <br>
+        <a target="_blank" href="https://playoverwatch.com"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        headphones: `I'm still new to the audio world, 
+        but I already have a little collection of headphones. <br>
+        It is very enjoyable to listen to some music with different hardware.
+        Do you know that you can purchase a good pair of headphones for just 20 USD? Check it out: <br>
+        <a target="_blank" href="http://www.gearbest.com/headsets/pp_270782.html?vip=401831"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        talk: `Do you like talking? Me too! <br> 
+        I really enjoy discussing new ideas and help others. <br>
+        I'm also adicted to "cases" and read a lot in Quora and Medium. <br>
+        <a target="_blank" href="https://www.quora.com"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        icetea: `If you want to see me happy a cup of iced tea is a great start! <br> 
+        I also enjoy to go out for a drink and a talk. <br>
+        If I have the time, I try to do mix stuff with tea... <br>
+        <a target="_blank" href="https://www.reddit.com/r/tea/"><i class="fas fa-external-link-alt"></i></a>
+        `
+      },
+      professional: {
+        title: 'Professional',
+        subtitle: 'A collection of professional tools and skills.',
+        whoami: `I'm a developer "graduated" in Computer Sciente by ICMC from Universidade de Sao Paulo. <br>
+        Most of the things I learned and used on a day-to-day basis at work was out of my own curiosity. <br>
+        Motivation for me is a very important factor and my productivity depends a lot on it. <br>
+        `,
+        js: `Javascript is today the language that I am most confident with. <br>
+        Node.js is an amazing tool in the right hands! <br>
+        I like to write simple and elegant code, and I try to keep that standard in all my codes. <br>
+        <a target="_blank" href="https://nodejs.org/en/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        python: `Python is my language of choice to crunch some numbers and data, and also in image processing.<br>
+        Async and threads are very powerful under python and they are often ignored.<br>
+        I still want to improve my skills in this language. <br>
+        <a target="_blank" href="https://docs.python.org/3/library/asyncio.html"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        solidity: `Solidity is the language that moves the Ethereum ecosystem. <br>
+        It is a language that I put a lot of effort in understanding it, and I realized that without
+        basic concepts of criptocoins it is impossible to learn it.<br>
+        <a target="_blank" href="https://www.ethereum.org/greeter"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        c: `I learned programming in C. <br>
+        So C have a special place in this collection, although I dont't use it much today. <br>
+        Some projects in "CUDA" and "Concurrent Programming" I did in this language. <br>
+        <a target="_blank" href="http://devdocs.io/c/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        go: `I don't have any experience in GO. <br>
+        It is the language that I'm most curious about and I'm still a student. <br>
+        GO's logo is epic! <br>
+        <a target="_blank" href="https://golang.org/doc/install"><i class="fas fa-external-link-alt"></i></a>
+        `
+      },
+      toolbelt: {
+        title: 'Favorite Apps',
+        subtitle: 'List of programs that I really like to use!',
+        gitkraken: `I used GIT under the CLI, but then I discovered Gitkraken.<br>
+        Today I tell people about this amazing program for every developer that uses GIT and wants the job done. <br>
+        And soon we will have <a target="_blank" href="https://www.gitkraken.com/glo" class="neon">Gitkraken Glo<a>!<br>
+        <a target="_blank" href="https://www.gitkraken.com/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        sublime: `I already tried VIM, Notepad++ (old times), Brackets, Atom e some IDEs. I always go back to Sublime 
+        for his speed. I don't think of changing my text editor soon...<br>
+        <a target="_blank" href="https://www.sublimetext.com/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        hyper: `Imagine the customization power of the web inside a terminal... <br>
+        This is why I use Hyper, easy customizable terminal and damn pretty! <br>
+        <a target="_blank" href="https://hyper.is/"><i class="fas fa-external-link-alt"></i></a>
+        `
+      },
+      nodemodules: {
+        title: 'Node.js Modules',
+        subtitle: 'Bucket of libs that I enjoy.',
+        now: `I discovered "now" and Zeit's ecosystem by the end of 2017. <br>
+        The ease of deploy and scalability is outstanding! <br>
+        You can put an API to work with a URL in just 10 seconds. Check it out:<br>
+        <a target="_blank" href="https://zeit.co/now"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        micro: `Micro fast and modern! <br>
+        ES6 with "Async", "Await" and very lightweight. A fine job by <a target="_blank" href="https://zeit.co//">Zeit's</a> team. <br>
+        I use it in all my projects with "now".
+        <a target="_blank" href="https://zeit.co/micro"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        artillery: `A powerful lib to test the resilience of applications. <br>
+        It is very complete, and before this lib I writed lots of pythons scripts. <br>
+        The ideal is to setup a Digital Ocean dropplet with Artillery and fire at will! <br>
+        <a target="_blank" href="https://artillery.io/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        graphql: `I'm new to this technology, but it is very promising! <br>
+        I'm using it today in a few of my projects along with microservices <br>
+        Prisma is just astonishing! <br>
+        <a target="_blank" href="https://www.prisma.io/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        objection: `An ORM that don't cry all day. <br>
+        It gives back the power of SQL with very powerful querybuilders. <br>
+        It comes with no surprises in a clean way. <br>
+        <a target="_blank" href="https://vincit.github.io/objection.js/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        ava: `A modern and complete test framework. <br>
+        All my tests from projects I do with AVA. <br>
+        Using "request-promise" it is very good to write autonomous tests for the endpoints of an API. <br>
+        <a target="_blank" href="https://github.com/avajs/ava"><i class="fas fa-external-link-alt"></i></a>
+        `
+      }
+    }
+  },
+
+  ptbr: {
+    flag: '/assets/BR.png',
+    sections: {
+      personal: {
+        title: 'Pessoal',
+        subtitle: 'Algumas coisas sobre mim.',
+        pcmasterrace: `Sempre tive um PC em casa. <br>
+        Eu montei meu primeiro PC em 2010. <br>
+        PC Master Race FTW. <br>
+        <a target="_blank" href="https://www.reddit.com/r/pcmasterrace/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        crypto: `Eu Comecei com Litecoin e Bitcoin em 2012.
+        Só fui estudar a tecnologia a partir de 2015. <br>
+        Ethereum é sensacional! <br>
+        Se você me disser: "Eu não gosto de Bitcoin, já blockchain é muito interessante." eu morro discretamente por dentro.<br>
+        <a target="_blank" href="https://github.com/bitcoinbook/bitcoinbook"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        overwatch: `Eu gosto muito de Overwatch! <br>
+        Meu "Career High" é 4075. <br>
+        Me considero um flex player, mas jogo melhor de tank ou suporte. <br>
+        <a target="_blank" href="https://playoverwatch.com/pt-br"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        headphones: `Eu estou começando ainda no mundo do áudio. 
+        Mas já tenho uma coleção razoável de headphones. <br>
+        É muito divertido ouvir música com diferentes tipos de acessórios.
+        Você sabia que um bom headphone para começar custa apenas 20 dólares? Confere aí: <br>
+        <a target="_blank" href="http://www.gearbest.com/headsets/pp_270782.html?vip=401831"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        talk: `Você gosta de conversar? Eu também! <br> 
+        Gosto muito de discutir ideias e ajudar os outros. <br>
+        Sou viciado em cases e leio bastante sites como Quora e Medium.<br>
+        <a target="_blank" href="https://www.quora.com"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        icetea: `Se você quer me ver feliz um copo de chá gelado é um bom começo! <br> 
+        Também gosto muito de sair para beber alguma coisa e apenas conversar. <br>
+        Sempre que tenho algum tempo busco fazer alguma coisa com chá... <br>
+        <a target="_blank" href="https://www.reddit.com/r/tea/"><i class="fas fa-external-link-alt"></i></a>
+        `
+      },
+      professional: {
+        title: 'Profissional',
+        subtitle: 'Uma coleção de ferramentas e skills profissionais.',
+        whoami: `Eu sou um desenvolvedor "formado" em Ciências da Computação pelo ICMC da USP. <br>
+        A maioria das coisas que aprendi e uso no dia a dia do trabalho foi por curiosidade própria. <br>
+        Motivação pra mim é um fator muito importante e minha produtividade depende muito dela. <br>
+        `,
+        js: `Javasript é Hoje a linguagem que eu mais domino. <br>
+        Node.js é uma ferramenta fantástica! <br>
+        Gosto de fazer códigos simples e elegante, e me esforço para manter esse padrão em todos os meus programas. <br>
+        <a target="_blank" href="https://nodejs.org/en/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        python: `Python é a linguagem que eu uso para números e dados, e também para processamento de imagens.<br>
+        Async e threads são ferramentas muito poderosas em python.<br>
+        Ainda quero melhorar minhas habilidades nessa linguagem.<br>
+        <a target="_blank" href="https://docs.python.org/3/library/asyncio.html"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        solidity: `Solidity é a linguagem que movimenta todo o ecossistema Ethereum. <br>
+        É uma linguagem que eu me esforcei bastante para entender, e percebi que sem os conceitos 
+        básicos de criptomoedas não tem como aprendê-la. <br>
+        <a target="_blank" href="https://www.ethereum.org/greeter"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        c: `Eu aprendi programação em C. <br>
+        Portanto ela tem um lugar especial nessa lista, apesar de usá-la pouco hoje em dia. <br>
+        Alguns projetos em "CUDA" e "Programação Concorrente" eu fiz nessa linguagem. <br>
+        <a target="_blank" href="http://devdocs.io/c/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        go: `Eu ainda não tenho conhecimentos em GO. <br>
+        É a linguagem que eu mais tenho curiosidade e ainda sou um estudante nela. <br>
+        O logo de GO é épico! <br>
+        <a target="_blank" href="https://golang.org/doc/install"><i class="fas fa-external-link-alt"></i></a>
+        `
+      },
+      toolbelt: {
+        title: 'Programas Preferidos',
+        subtitle: 'Lista de programas que eu gosto muito de usar!',
+        gitkraken: `Eu usava GIT no terminal, mas aí descobri Gitkraken. <br>
+        Hoje eu indico esse programa fantástico para qualquer desenvolvedor que usa GIT e quer velocidade. <br>
+        E em breve teremos <a target="_blank" href="https://www.gitkraken.com/glo" class="neon">Gitkraken Glo<a>! <br>
+        <a target="_blank" href="https://www.gitkraken.com/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        sublime: `Eu já usei VIM, Notepad++ (old times), Brackets, Atom e algumas IDEs. Sempre volto a usar o Sublime 
+        por sua velocidade. Não acho que vou mudar de editor tão cedo...<br>
+        <a target="_blank" href="https://www.sublimetext.com/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        hyper: `Imagine o poder de customização da web dentro de um terminal... <br>
+        É por isso que eu uso Hyper, fácil de customizar e bonito! <br>
+        <a target="_blank" href="https://hyper.is/"><i class="fas fa-external-link-alt"></i></a>
+        `
+      },
+      nodemodules: {
+        title: 'Node.js Modules',
+        subtitle: 'Algumas bilbiotecas que eu gosto para Node.js.',
+        now: `Descobri now.sh e o ecossistema Zeit no final de 2017. <br>
+        A facilidade de deploy e escalabilidade é tenebrosa! <br>
+        Você pode colocar uma API funcionando e com uma URL em apenas 10 segundos. Confere aí: <br>
+        <a target="_blank" href="https://zeit.co/now"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        micro: `Micro é rápido e moderno! <br>
+        ES6 com "Async" e "Await" e muito leve. Um belo trabalho da <a target="_blank" href="https://zeit.co//">Zeit</a>. <br>
+        Uso em todos os novos projetos junto com "now".
+        <a target="_blank" href="https://zeit.co/micro"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        artillery: `Uma biblioteca para testar a resiliência de aplicações. <br>
+        É extremamente completa, antes dela escrevia scripts em python. <br>
+        O ideal é colocar um servidor na Digital Ocean com Artillery e disparar a vontade. <br>
+        <a target="_blank" href="https://artillery.io/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        graphql: `Ainda sou novo nessa tecnologia, mas ela é  bem promissora!<br>
+        Estou usando em alguns projetos junto com microsserviços. <br>
+        Prisma é simplesmente fenomenal! <br>
+        <a target="_blank" href="https://www.prisma.io"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        objection: `Um ORM que não fica de mimimi. <br>
+        Devolve o poder de volta ao SQL com querybuilders muito poderosos. <br>
+        É um ORM que não vem com surpresas e de maneira limpa. <br>
+        <a target="_blank" href="https://vincit.github.io/objection.js/"><i class="fas fa-external-link-alt"></i></a>
+        `,
+        ava: `Um framework de testes completo e moderno. <br>
+        Todos os testes nos meus projetos atuais eu faço com AVA. <br>
+        Usando "request-promise" é bom para testar de maneira automática qualquer endpoint de uma API. <br>
+        <a target="_blank" href="https://github.com/avajs/ava"><i class="fas fa-external-link-alt"></i></a>
+        `
+      }
+    }
+  }
+}
